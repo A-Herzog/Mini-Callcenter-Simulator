3835eb84efc08ba518f99c66c7d080b17a3699cf
Alexander Herzog <alexander.herzog@gmx.de>
2023-03-07 08:14:58 +0100

    Prepare release of version 5.9.232

--- Simulator/src/main/java/ui/MainPanel.java
+++ Simulator/src/main/java/ui/MainPanel.java
@@ -118,7 +118,7 @@ public class MainPanel extends MainPanelBase {
 	/**
 	 * Programmversion
 	 */
-	public static final String systemVersion="5.8.231";
+	public static final String systemVersion="5.9.232";
 
 	/**
 	 * Bezeichnung für "ungespeichertes Modell" in der Titelzeile für ein neues Modell, welches noch keinen Namen besitzt
